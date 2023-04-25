c572be20e3ff137003a2dc00e5677e44a998456b
SherTi <tulkunovsher69@gmail.com>
2023-04-25 07:54:56 +0500

    mobila optimization

--- main-file.js
+++ main-file.js
@@ -34,32 +34,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
 
     });
-    // breakpoints: {
-    //                 1080:{
-    //                     slidesPerView: 2,
-    //                     slidesPerGroup: 1,
-    //                 },
-    //                  320: {
-    //                      slidesPerView: 1,
-    //                      slidesPerGroup: 1,
-    //                      loop:true
-    //                 },
-    //                 400: {
-    //                     slidesPerView: 1,
-    //                     slidesPerGroup: 1,
-    //                 },
-    //                 430: {
-    //                     slidesPerView: 1,
-    //                     slidesPerGroup: 1,
-    //                 },
-    //                 2560:{
-    //                   slidesPerView: 2,
-    //                   slidesPerGroup : 1,
-    //                 },
-    const swiper1 = new Swiper('.certificate-slider' , {
+    const certificate_slider = new Swiper('.certificate-slider' , {
         direction: 'horizontal',
-        slidesPerView: 4,
-        slidesPerGroup: 1,
         loop: true,
         effect: 'slide',
         width: 1200,
@@ -75,26 +51,32 @@ window.addEventListener('DOMContentLoaded', (event) => {
             el: "#dots-container",
             bulletClass: "s-dot",
         },
-    });
-    const swiper2 = new Swiper('.certificate-slider-p' , {
-        direction: 'horizontal',
-        slidesPerView: 4,
-        slidesPerGroup: 1,
-        loop: true,
-        effect: 'slide',
-        width: 1150,
-        navigation: {
-            nextEl: '.right-btn-n',
-            prevEl: '.left-btn-n',
-        },
-        pagination: {
-            clickable: true,
-            enabled: true,
-            type: "bullets",
-            bulletActiveClass: "slider-dots-active",
-            el: "#dots-container",
-            bulletClass: "s-dot",
-        },
+        breakpoints:{
+            1080:{
+                slidesPerView: 4,
+                slidesPerGroup: 1,
+                width:1200,
+                loop: true,
+            },
+            425:{
+                slidesPerView:4,
+                slidesPerGroup:1,
+                width:750,
+                loop: true,
+            },
+            375:{
+                slidesPerView:5,
+                slidesPerGroup:1,
+                width:750,
+                loop: true,
+            },
+            320:{
+                slidesPerView:5,
+                slidesPerGroup:1,
+                width:750,
+                loop: true,
+            }
+        }
     });
     const nav = document.querySelector('.catalog-btn')
     const menu = document.querySelector('.container-nav')
